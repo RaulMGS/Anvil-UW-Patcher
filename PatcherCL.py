f7df0b912fe4ea54cc4209532e5ff47fbca433df
Raul Turcuș <46862690+RaulMGS@users.noreply.github.com>
2021-10-16 13:05:24 +0300

    Fix import issues with packer

--- PatcherCL.py
+++ PatcherCL.py
@@ -1,5 +1,5 @@
 from os import error
-import sys
+from sys import argv, exit
 import Util.FileHandling as filehandler
 
 # 32:9 - 39 8e 63 40
@@ -10,12 +10,12 @@ BYTES_21_9 = b"\x9a\x99\x19\x40"
 BYTES_16_9 = b"#\x39\x8E\xE3\x3F"
 
 # Check if executable file exists
-if not filehandler.file_exists(sys.argv[1]):
+if not filehandler.file_exists(argv[1]):
     print("Invalid input file")
     exit()
 
 # Read original file
-byteData = filehandler.file_read_bytes(sys.argv[1])
+byteData = filehandler.file_read_bytes(argv[1])
 
 # Check if the file is in original state
 if byteData.find(BYTES_16_9) == -1:
@@ -23,12 +23,12 @@ if byteData.find(BYTES_16_9) == -1:
     exit()
 
 # Save a backup of the original
-filehandler.file_write_bytes(sys.argv[1] + ".bak", byteData)
+filehandler.file_write_bytes(argv[1] + ".bak", byteData)
 
-if sys.argv[2] == "21:9":
+if argv[2] == "21:9":
     print("Patching to 21:9")
     byteData = byteData.replace(BYTES_16_9, BYTES_21_9)
-elif sys.argv[2] == "32:9":
+elif argv[2] == "32:9":
     byteData = byteData.replace(BYTES_16_9, BYTES_32_9)
     print("Patching to 32:9")
 else:
